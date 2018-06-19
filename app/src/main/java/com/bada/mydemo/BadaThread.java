70be9889a39f428347613791aafc2091b827ddb5
iamkya <shadowpriest.kya@gmail.com>
2018-06-19 18:16:15 +0800

    test run ocr

--- app/src/main/java/com/bada/mydemo/BadaThread.java
+++ app/src/main/java/com/bada/mydemo/BadaThread.java
@@ -2,9 +2,17 @@ package com.bada.mydemo;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Environment;
 
 import com.bada.mydemo.rect.FocusRect;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.OutputStream;
 
 public class BadaThread extends Thread {
@@ -12,11 +20,20 @@ public class BadaThread extends Thread {
     Process shell = null;
 
     private static final String screen_cap_path = "/sdcard/colorPickerTemp.png";
+    private static final String screen_test_path = "/sdcard/bada/main2.jpg";
+
     @Override
     public void run() {
 
         try {
-            shell = Runtime.getRuntime().exec("su", null,null);
+            shell = Runtime.getRuntime().exec("gtsu", null,null);
+
+            //while (true) {
+
+                findStr();
+
+            //}
+
         }catch (Throwable e){
             e.printStackTrace();
         }
@@ -26,30 +43,77 @@ public class BadaThread extends Thread {
 
     static final FocusRect friendInfo = new FocusRect(0, 0, 1000, 200);
 
-    boolean capture() {
+//    boolean capture() {
+//        try {
+//            OutputStream os = null;
+//
+//            os = shell.getOutputStream();
+//            os.write(("/system/bin/screencap -p " + screen_cap_path).getBytes("ASCII"));
+//            os.flush();
+//
+//            os.close();
+//            shell.waitFor();
+//
+//            Bitmap screen = BitmapFactory.decodeFile(screen_cap_path);
+//
+//            Bitmap part = getPart(screen);
+//
+//        }catch (Throwable e) {
+//            e.printStackTrace();
+//        }
+//
+//        return false;
+//    }
+
+    ColorMatrix colorMatrix;
+
+    public Bitmap convertGray(Bitmap bitmap3) {
+        colorMatrix = new ColorMatrix();
+        colorMatrix.setSaturation(0);
+        ColorMatrixColorFilter filter = new ColorMatrixColorFilter(colorMatrix);
+
+        Paint paint = new Paint();
+        paint.setColorFilter(filter);
+        Bitmap result = Bitmap.createBitmap(bitmap3.getWidth(), bitmap3.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(result);
+
+        canvas.drawBitmap(bitmap3, 0, 0, paint);
+        return result;
+    }
+
+
+    boolean findStr() {
+        Bitmap screen = BitmapFactory.decodeFile(screen_test_path);
+
+        Rect rect = new Rect(1300, 0, 1920, 75 + 130);
+
+        Bitmap part = getPart(screen, rect);
+
+//        Bitmap grey = convertGray(part);
+        Bitmap grey = part;
+
         try {
-            OutputStream os = null;
+            File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "part.jpg");
+            FileOutputStream fo = new FileOutputStream(file);
 
-            os = shell.getOutputStream();
-            os.write(("/system/bin/screencap -p " + screen_cap_path).getBytes("ASCII"));
-            os.flush();
-
-            os.close();
-            shell.waitFor();
-
-            Bitmap screen = BitmapFactory.decodeFile(screen_cap_path);
-
-            Bitmap part = getPart(screen, friendInfo);
-
+            grey.compress(Bitmap.CompressFormat.JPEG, 100, fo);
+            fo.flush();
+            fo.close();
 
         }catch (Throwable e) {
             e.printStackTrace();
         }
 
-        return false;
-    }
+        OCRManager.getInstance().doOcr(grey, null);
 
-    private Bitmap getPart(Bitmap screen, FocusRect rect) {
-        return Bitmap.createBitmap(screen, rect.getTopX(), rect.getTopY(), rect.getWidth(), rect.getHeight());
+        part.recycle();
+        screen.recycle();
+
+        return true;
+    }
+    //new Rect(1237, 70 , 1709, 150)
+    private Bitmap getPart(Bitmap screen, Rect rect) {
+
+        return Bitmap.createBitmap(screen, rect.left, rect.top, rect.width(), rect.height());
     }
 }
