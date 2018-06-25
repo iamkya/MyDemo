2715669597401b6bfb1ecede5042eec90af70151
zhenqiang.gao <shadowpriest.kya@gmail.com>
2018-06-25 18:22:47 +0800

    111

--- app/src/main/java/com/bada/mydemo/AnotherThread.java
+++ app/src/main/java/com/bada/mydemo/AnotherThread.java
@@ -13,20 +13,23 @@ public class AnotherThread extends BaseThread {
 
     private static final String screen_cap_file =  "/sdcard/colorPickerTemp.png";
 
-    private RandomRect comfirmExpedition = new RandomRect(1000, 710, 200, 70);
+    private RandomRect confirmExpedition = new RandomRect(1000, 710, 200, 70);
     @Override
     public void run() {
         super.run();
 
         try {
 
-            sleep(15 * 1000);
 
+//            sleep(15 * 1000);
+//
 //            while (true) {
 //
-                checkExpedition();
+//                checkExpedition(false);
 //
-//                break;
+//                checkExpedition(true);
+//
+                startCombat();
 //            }
 
         }catch (Throwable e) {
@@ -34,21 +37,64 @@ public class AnotherThread extends BaseThread {
         }
     }
 
-    private void checkExpedition() throws Exception{
+    private static final int maxCombatCount = 4;
+
+    private RandomRect combatRect = new RandomRect();
+    private RandomRect zeroTwoRect = new RandomRect();
+    private RandomRect normalCombatRect = new RandomRect();
+
+    private RandomRect startPointLeftRect = new RandomRect(); //airport
+    private RandomRect startPointRightRect = new RandomRect(); //command center
+
+    private RandomRect groupFormationRect = new RandomRect();
+
+    private RandomRect selectGroup1AtDeploy = new RandomRect();
+    private RandomRect selectGroup2AtDeploy = new RandomRect();
+
+    void startCombat(){
+
+        for (int i = 0; i < maxCombatCount ; i ++) {
+
+            doCombat(i);
+        }
+
+
+    }
+
+    private void doCombat(int index) {
+
+        click2(combatRect);
+
+        click2(zeroTwoRect);
+
+        click2(normalCombatRect);
+
+        click2(startPointRightRect);//点机场
+
+        //弹出队伍选择
+        click2(groupFormationRect);
+
+        int fightGroup = index%2;
+        int waitGroup = (index + 1)%2;
+
+
+    }
+
+    private void checkExpedition(boolean keepChecking) throws Exception{
 
         int i = 0;
         while (true) {
 
             if(shouldClick()) {
                 i = 0;
-                click2(comfirmExpedition);
+                click2(confirmExpedition);
             }
             else{
                 sleep(getSleepTime() * 1000);
                 i++;
             }
 
-            if(i > 11) {
+            if(!keepChecking && i > 11) {
                 break;
             }
         }
